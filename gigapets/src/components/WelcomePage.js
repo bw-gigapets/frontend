a7b96034d5616b556e344fb09e0496826c9d0485
unknown <jabaritf@gmail.com>
2020-02-03 17:08:13 -0500

    worked on WelcomePage and made it functional

--- gigapets/src/components/WelcomePage.js
+++ gigapets/src/components/WelcomePage.js
@@ -1,1 +1,62 @@
-import react from 'react'
+import React from "react";
+import styled from 'styled-components';
+import {Button} from "semantic-ui-react";
+import { Link } from 'react-router-dom';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Nav = styled.nav`
+
+box-shadow: 2px 2px 2px #888888;
+background-color: #ADFFB5;
+display: flex;
+flex-direction: row;
+flex-wrap; nowrap;
+justify-content: flex-end;
+align-items: baseline;
+margin-bottom: 5%;
+`
+
+const A = styled.a`
+color: palevioletred;
+font-size: 1.2em;
+
+`
+
+
+const Imge= styled.img`
+    margin-left:10%;
+
+`
+
+const Buttn = styled.button`
+    color: palevioletred;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+`
+
+export default function WelcomePage() {
+    return (
+      <section className="welcome-page">
+        <Nav>
+        <Link to="/about">
+            <A>About</A>
+        </Link>
+        <Link to="/login">
+            <Buttn>Login/Sign-up</Buttn>
+        </Link>
+        </Nav>
+        <header>
+          <Title>Welcome to Gigapets! The educational nutritional Tool for kids!</Title>
+          <Imge src="https://www.misskatecuttables.com/uploads/shopping_cart/8089/large_cute-monsters.png"/>
+        </header>
+      </section>
+    )
+  }
