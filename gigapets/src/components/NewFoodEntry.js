898090e0001cff35a274b4ec5e8ad0aad3ab0fd2
JulisaDiego <julisadiego@yahoo.com>
2020-02-06 21:54:39 -0500

    committing to pull and merge

---
+++ gigapets/src/components/NewFoodEntry.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import NavHeader from "./Layout/Navigation";
+
+const AddFood = () => {
+    return (
+        <>
+        <NavHeader/>
+          
+      </>
+    );
+  };
+  
+  export default AddFood;
